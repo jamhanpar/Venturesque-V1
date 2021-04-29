1ecbf7faaf295f833f1db00fa2613a5f3cab0057
James <james.hansung.park@gmail.com>
2021-04-29 14:48:16 -0400

    updated questions patch api route

--- backend/routes/api/questions.js
+++ backend/routes/api/questions.js
@@ -25,6 +25,12 @@ router.post('/', (req, res) => {
         .catch(err => res.status(404).json(err))
 })
 
+router.patch('/:question_id', (req, res) => {
+    Question.findOneAndUpdate({ _id: req.params.question_id}, {$set: req.body}, {new: true, useFindAndModify: false})
+        .then(question => res.json(question))
+            .catch(err => res.status(404).json(err))
+})
+
 router.delete("/:question_id", (req, res) => {
   Question.findOneAndDelete({ _id: req.params.question_id})
     .then(question => res.json({_id: question._id}))
