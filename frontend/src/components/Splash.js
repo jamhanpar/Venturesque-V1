30a072d36848d849ed6d01113d6ab167a6c9eb19
James <james.hansung.park@gmail.com>
2021-04-29 17:28:49 -0400

    🎨: updated form css

--- frontend/src/components/Splash.js
+++ frontend/src/components/Splash.js
@@ -1,25 +1,46 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import './stylesheets/splash.css';
 
 const Splash = () => {
+    const [location, setLocation] = useState('');
+    const [date, setDate] = useState("");
+    const [friend, setFriend] = useState('');
+
     return (
-        <div class='app-container'>
-            <header>Header</header>
-            <nav>Navbar</nav>
-            <section>Section</section>
-            <article>Article</article>
-            <aside>Side Menu / Article</aside>
-            <footer>Footer</footer>
-        </div>
-    )
+      <div class="app-container">
+        {/* <header>Header</header>
+        <nav>Navbar</nav>
+        <section>Section</section>
+        <article>Article</article>
+        <aside>Side Menu / Article</aside>
+        <footer>Footer</footer> */}
+
+        <section class="search-input">
+            <form>
+                <div>
+                    <label for="location">The best first date near </label>  
+                    <input type="text" id="location" placeholder='address, city, zipcode' onChange={() => setLocation()}/>
+                </div>
+                <div>
+                    <input type="text" id="date" placeholder='date' onChange={() => setDate()}/>
+                </div>
+                <div>
+                    <label for="friend">with </label>
+                    <input type="text" id="friend" placeholder='friend' onChange={() => setFriend()}/>
+                </div>
+            </form>
+        </section>
+      </div>
+    );
 }
 
-const msp = () => ({
+// const msp = () => ({
 
-});
+// });
 
-const mdp = dispatch => ({
+// const mdp = dispatch => ({
     
-});
+// });
 
 export default connect(null, null)(Splash);
