ee6a6d04c969fd66de6a1db48ae105481d2b5cf0
James <james.hansung.park@gmail.com>
2021-05-04 17:42:25 -0400

    ❌: testing not working yelp api

---
+++ backend/routes/api/yelp.js
@@ -0,0 +1,6 @@
+const express = require("express");
+require('dotenv').config();
+
+const yelp_api_key = process.env.REACT_APP_YELP_API_KEY;
+
+// review questions routes
--- src/actions/yelp_actions.js
+++ src/actions/yelp_actions.js
@@ -2,13 +2,60 @@ import { createAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 export const receiveRestaurants = createAction('RECEIVE_RESTAURANTS');
+export const yelpBaseURL = 'https://api.yelp.com/v3/'
 
-export const fetchRestaurants = () => async dispatch => {
-    try {
-        const res = await axios.get('http://localhost:5000/api/questions')
-        dispatch(receiveRestaurants(res.data))
-    }
-    catch (err) {
-        console.log(err)
-    }
+// export const fetchRestaurants = (term, location) => async dispatch => {
+//     try {
+//         const res = await axios.get(`${yelpBaseURL}/business/search`, { 
+//             params: {
+//                 location,
+//                 term
+//             }
+//         }, {
+//             headers: {
+//                 Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`
+//             }
+//         });
+//         dispatch(receiveRestaurants(res.data));
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
+
+const yelp = require('yelp-fusion');
+const client = yelp.client(process.env.REACT_APP_YELP_API_KEY);
+
+export const fetchRestaurants = (term, location) => async dispatch => { 
+    client.search({ term, location })
+        .then( res => dispatch(receiveRestaurants(res.data)))
+        .catch(err => console.log(err));
 }
+
+
+// Example
+// let YelpREST = axios.create({
+//     baseURL: "https://api.yelp.com/v3/",
+//     headers: {
+//         Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
+//         "Content-type": "application/json"
+//     },
+// })
+
+// yelpREST(ENDPOINT, { params: { key: value } })
+//     .then(({ data }) => {
+//         // do something
+//     })
+
+// yelpREST("/business/search", {
+//     params: {
+//         location: "kyoto",
+//         term: "coffee",
+//         limit: 10,
+//     }
+// }).then(({ data }) => {
+//     let { business } = data
+//     businesses.forEach(business => {
+//         console.log("Name: ", business.name)
+//     }) 
+// })
