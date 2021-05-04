9a97be5180570944a0e8f97789029b3f7e4483ad
James <james.hansung.park@gmail.com>
2021-05-04 15:17:17 -0400

    imported hookrouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Provider } from 'react-redux';
 import { HashRouter, Switch, Route } from 'react-router-dom';
+import { useRoutes } from 'hookrouter';
 import './App.css';
 
 import Splash from './components/Splash';
+import LoginSignup from './components/LoginSignup';
 
 function App({ store }) {
   return (
@@ -10,7 +12,9 @@ function App({ store }) {
       <HashRouter>
         <div className="App">
           <Switch>
-            <Route path='/' component={Splash}/>
+            <Route path="/home" component={Splash} />
+            <Route path="/login" component={LoginSignup} />
+            {/* <Route path="/signup" component={LoginSignup} /> */}
           </Switch>
         </div>
       </HashRouter>
