163622ad9a765c09b9545a8a940d7cd1eb4817b0
James <james.hansung.park@gmail.com>
2021-05-04 21:47:21 -0400

    🎨: updated splash formatting

--- src/components/Splash.jsx
+++ src/components/Splash.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { fetchRestaurants } from '../actions/yelp_actions';
-import './stylesheets/splash.css';
+import './stylesheets/splash.scss';
 
 const Splash = () => {
     const [searchTerm, setSearchTerm] = useState('Restaurants');
     const [location, setLocation] = useState('Hoboken');
-    const [date, setDate] = useState(`${new Date().toLocaleDateString()}`);
+    const [date, setDate] = useState('');
     const [friend, setFriend] = useState('Vivian Chen');    
     
     const select = (el, all = false) => {
@@ -19,31 +19,26 @@ const Splash = () => {
     }
 
     const onSubmit = () => {
-        console.log("inputs submitted");
+        console.log('inputs submitted');
     }
 
     return (
-        <div className="app-container">
-            <section className="search-input">
-                <form onSubmit={onSubmit}>
-                    <div className="search-input-item">
-                        <label htmlFor="location">The best first date near </label>    
-                        <input type="text" id="location" placeholder='address, city, zipcode' onChange={() => setLocation()} value={location}/>
-                    </div>
-                    <div className="search-input-item">
-                        <input type="text" id="date" placeholder='date' onChange={() => setDate()} value={date}/>
-                    </div>
-                    <div className="search-input-item">
-                        <label htmlFor="friend">with </label>
-                        <input type="text" id="friend's email" placeholder='friend' onChange={() => setFriend()} value={friend}/>
-                    </div>
-                    <div className="search-input-item">
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
-            </section>
-            {/* <footer>Footer</footer> */}
-        </div>
+        <section className='search-input'>
+            <form onSubmit={onSubmit}>
+                <div className='search-input-item'>
+                    <label className='landing-page-text' htmlFor='location'>the best <span className='rotate-text'>first</span> date near </label>    
+                    <input className='landing-page-input' type='text' id='location' placeholder='address, city, zipcode' onChange={() => setLocation()} value={location}/>
+                </div>
+                <div className='search-input-item'>
+                    <input className='landing-page-date-input' type='date' id='date' placeholder='date' onChange={() => setDate()} value={date}/>
+                </div>
+                <div className='search-input-item'>
+                    <label className='landing-page-text' htmlFor='friend'>with </label>
+                    <input className='landing-page-input' type='text' id='friend' placeholder='friend' onChange={() => setFriend()} value={friend}/>
+                </div>
+                <button className='generate-button' type='submit'>Generate</button>
+            </form>
+        </section>
     );
 }
 
