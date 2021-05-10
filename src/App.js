9301774ec27257654d50d414ceffe758b19df5bd
James <james.hansung.park@gmail.com>
2021-05-10 19:58:02 -0400

    🚚: updated routes and switched to react-router-dom from hookrouter library

--- src/App.js
+++ src/App.js
@@ -8,29 +8,31 @@ import { Nav } from './components/Navbar';
 
 // Pages
 import Splash from './components/Splash.jsx';
-import PageNotFound from './pages/PageNotFound';
+// import PageNotFound from './pages/PageNotFound';
 import { LoginSignup } from './pages/LoginSignup';
 import { Results } from './pages/Results';
 
 const routes = {
   // '/Venturesque': () => <Splash />,
   '/auth/:toggle': ({ toggle }) => <LoginSignup toggle={ toggle } />,
-  '/results?term=:term&location=:location': ({ term, location }) => <Results />
+  '/search?term=:term&location=:location': ({ term, location }) => <Results term={ term } location={ location } />
 }
 
 function App({ store }) {
-  const routeMatch = useRoutes(routes);
+  // const routeMatch = useRoutes(routes);
 
   return (
-    <Provider store={store} className="provider">
+    <Provider store={store}>
       <div className="App">
         <Nav />
         {/* {routeMatch || <PageNotFound />} */}
-        {routeMatch}
+        {/* {routeMatch} */}
 
         <Switch>
           <Route exact path='/' component={Splash}/>
-          {/* <Route path='/results' component={Results}/> */}
+          <Route path='/search/term=:term&location=:location' component={Results}/>
+          <Route path='/results' component={Results}/>
+          <Route path='/auth/:toggle' component={LoginSignup}/>
         </Switch>
       </div>
     </Provider>
