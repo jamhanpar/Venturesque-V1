5c65daf6990868b91b1e4af2ab16713abb02a3eb
James <james.hansung.park@gmail.com>
2021-05-10 19:59:05 -0400

    🧪: testing r converted react router dom nav links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { A } from 'hookrouter';
+// import { A } from 'hookrouter';
+import { Link } from 'react-router-dom';
 import logo from './../assets/img/temp-logo.png';
 import "./stylesheets/navbar.scss";
 
 export const Nav = () => {
     return (
         <div className="nav-container">
-            <A href="/"><img className="logo" src={logo} alt="App Logo" /></A>
+            <Link to="/"><img className="logo" src={logo} alt="App Logo" /></Link>
+            {/* <A href="/"><img className="logo" src={logo} alt="App Logo" /></A> */}
             <ul className="auth-nav-container">
-                <li><A className="auth-nav-link login-button" href="/results">Results</A></li>
-                <li><A className="auth-nav-link login-button" href="/auth/login">Log In</A></li>
-                <li><A className="auth-nav-link signup-button" href="/auth/signup">Sign Up</A></li>
+                <Link className="auth-nav-link login-button" to="/results">Results</Link>
+                <Link className="auth-nav-link login-button" to="/auth/login">Log In</Link>
+                <Link className="auth-nav-link signup-button" to="/auth/signup">Sign Up</Link>
+                {/* <li><A className="auth-nav-link login-button" href="/results">Results</A></li> */}
+                {/* <li><A className="auth-nav-link login-button" href="/auth/login">Log In</A></li>
+                <li><A className="auth-nav-link signup-button" href="/auth/signup">Sign Up</A></li> */}
             </ul>        
         </div>
     );
