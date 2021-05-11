10dd2f54ff87078ca776af395c1e056bd2297548
James <james.hansung.park@gmail.com>
2021-05-10 20:00:39 -0400

    🔧: updated links using react-router-dom

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { AuthForm } from '../components/AuthForm';
+import { useParams } from 'react-router-dom';
 
-export const LoginSignup = ({ toggle }) => {
+export const LoginSignup = () => {
+    const { toggle } = useParams();
+    
     return <AuthForm toggle={ toggle } />
 };
