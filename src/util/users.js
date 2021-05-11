36ffaa579b2b0ca4751f05fdcb6094048c1082aa
James <james.hansung.park@gmail.com>
2021-05-11 18:05:34 -0400

    🐛: user api does not work as expected

---
+++ src/util/users.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const receiveUsername = (name) => {
+    return {
+        type: "RECEIVE_USER",
+        name //email
+    }
+}
+
+export const getUsername = () => {
+    let data;
+    return axios
+        .get("https://randomuser.me/api")
+        .then((result) => {
+            data = result;
+        })
+        .catch(err => console.log(err));
+    return data
+}
+
+// export const fetchQuestion = () => async dispatch => {
+//     try {
+//         const res = await axios.get('http://localhost:5000/api/questions')
+//         dispatch(receiveQuestions(res.data))
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
