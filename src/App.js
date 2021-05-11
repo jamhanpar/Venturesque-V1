33900455a7da0c03bea97bf91a99aa2abc2d6c45
James <james.hansung.park@gmail.com>
2021-05-11 17:38:36 -0400

    🔧: minor code formatting

--- src/App.js
+++ src/App.js
@@ -10,18 +10,16 @@ import Splash from './components/Splash.jsx';
 import { LoginSignup } from './pages/LoginSignup';
 import { Results } from './pages/Results';
 
-export const App = ({ store }) => {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <Nav />
-        <Switch>
-          <Route exact path='/' component={Splash}/>
-          <Route path='/search/term=:term&location=:location' component={Results}/>
-          <Route path='/results' component={Results}/>
-          <Route path='/auth/:toggle' component={LoginSignup}/>
-        </Switch>
-      </Provider>
-    </div>
-  );
-}
+export const App = ({ store }) => (
+  <div className="App">
+    <Provider store={store}>
+      <Nav />
+      <Switch>
+        <Route exact path='/' component={Splash}/>
+        <Route path='/search/term=:term&location=:location' component={Results}/>
+        <Route path='/results' component={Results}/>
+        <Route path='/auth/:toggle' component={LoginSignup}/>
+      </Switch>
+    </Provider>
+  </div>
+);
