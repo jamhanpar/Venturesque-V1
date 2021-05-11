b4c6cc4ed2e0e79769c8e29aa6e7a0684190c6aa
James <james.hansung.park@gmail.com>
2021-05-11 17:20:21 -0400

    : passed in yelp restaurants as props t to search results component

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -4,14 +4,15 @@ import { SearchResult } from './SearchResult';
 export function SearchResults(props) {
     // console.log(props.businesses);
 
-    // if(!props.businesses ||  !props.businesses.length) return (<div></div>);
+    debugger
 
-    // const searchResults = props.businesses.map(business => <SearchResult key={business.id} business={business} />)
+    if(!props.businesses ||  !props.businesses.length) return (<div></div>);
+
+    const searchResults = props.businesses.map(business => <SearchResult key={business.id} business={business} />)
     
     return (
         <div>
-            {/* {searchResults} */}
-            <SearchResult />
+            {searchResults}
         </div>
     )
 }
