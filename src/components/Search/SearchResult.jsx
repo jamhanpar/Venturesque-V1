5a72f4e831225bc17351caa4cef2e91810f739dd
James <james.hansung.park@gmail.com>
2021-05-11 18:45:18 -0400

    🎨: formatting restaurant card

--- src/components/Search/SearchResult.jsx
+++ src/components/Search/SearchResult.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import "../stylesheets/searchResultItem.scss";
 
 export function SearchResult(props) {    
-    if (!props.business) return;
+    if (!props.business) {
+        return (
+            <div className='search-result-item-card'>
+                <img src='https://via.placeholder.com/468x60' alt="business"/>
+                <div>
+                    <h2>Business Name</h2>
+                    <div>Business Rating</div>
+                    <p>$$ <span>Burgers</span> <span>Fast Food</span></p>
+                </div>
+                <div>Business Contact & Address</div>
+            </div>
+        )
+    }
 
     return (
+      <div className="search-result-item-card">
+        <img className="restaurant-img" src={props.business.image_url} alt="business" />
         <div>
-            <img src={props.business.image_url} alt="business"/>
-            <div>
-                <h2>{props.business.name}</h2>
-                <div>Business Rating</div>
-                <p>$$ <span>Burgers</span> <span>Fast Food</span></p>
-            </div>
-            <div>Business Contact & Address</div>
+          <h2>{props.business.name}</h2>
+          <div>Business Rating</div>
+          <p>
+            $$ <span>Burgers</span> <span>Fast Food</span>
+          </p>
         </div>
-    )
+        <div>Business Contact & Address</div>
+      </div>
+    );
 }
