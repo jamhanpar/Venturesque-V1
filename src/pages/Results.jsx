b6b5f0548c3ee851a2af94dd70a27a2f1ecb8da1
James <james.hansung.park@gmail.com>
2021-05-11 17:21:34 -0400

    🍕: shows restaurants on results page, 🐛: need to manually activate cors-anywhere, need a workaround

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
+import { connect } from "react-redux";
 // import useReactRouter from 'use-react-router';
 import { useParams } from 'react-router-dom';
 import { Search } from '../components/Search/Search';
 import { SearchBar } from '../components/SearchBar';
-import { fetchRestaurants } from "../actions/yelp_actions";
+import { fetchRestaurants } from '../actions/yelp_actions';
+import { fetchWeather, getUsername } from '../actions/weather_actions';
+import { fetchUser } from '../actions/user_action';
+import { useBusinessSearch } from '../hooks/yelp-api/useBusinessSearch';
+import { SearchResults } from '../components/Search/SearchResults';
 
-export const Results = () => {
+export const Results = ({ fetchUser }) => {
     // const { location } = useReactRouter();
     // console.log(location);
     // const params = new URLSearchParams(location.search);
     // const termParam = params.get('term');
     // const locationParam = params.get('location');
     const { term, location } = useParams();
+    const [businesses, amountResults, searchParams, setSearchParams] = useBusinessSearch(term, location);
+    console.log(businesses);
+    const firstBusiness = businesses[0]
+    console.log(firstBusiness);
 
-    console.log(`${process.env.YELP_API_KEY}`);    
+    debugger
 
     return (
         <div>
-            <p>{term}</p>
-            <p>{location}</p>
+            {/* <p>{term}</p>
+            <p>{location}</p> */}
             <button onClick={() => fetchRestaurants(term, location)}>Fetch Restaurants</button>
-            <h1>Hello</h1>
-            <Search />
-            <SearchBar />
-            {/* <p>{termParam}</p>
-            <p>{locationParam}</p> */}
+            <button onClick={() => fetchWeather()}>Fetch Weather</button>
+            <button onClick={() => fetchUser()}>Fetch User</button>
+            <SearchResults businesses={businesses} />
+            {/* <SearchBar /> */}
         </div>
     )
 }
+
+// const msp = state => {
+//     debugger
+//     return {
+//         user: state,
+//     }
+// };
+
+// const mdp = (dispatch) => ({
+//     fetchUser: () => dispatch(fetchUser()),
+// });
+
+export default connect(null, null)(Results);
