a5f87007ed28a2030f3a3e36ff944cb02a4a1c82
James <james.hansung.park@gmail.com>
2021-05-13 15:21:36 -0400

    🎉: created user http request function

--- src/util/users.js
+++ src/util/users.js
@@ -1,21 +1,25 @@
 import axios from 'axios';
 
-export const receiveUsername = (name) => {
-    return {
-        type: "RECEIVE_USER",
-        name //email
-    }
-}
+// export const receiveUsername = (name) => {
+//     return {
+//         type: "RECEIVE_USER",
+//         name //email
+//     }
+// }
 
-export const getUsername = () => {
-    let data;
-    return axios
-        .get("https://randomuser.me/api")
-        .then((result) => {
-            data = result;
-        })
-        .catch(err => console.log(err));
-    return data
+// export const getUsername = () => {
+//     let data;
+//     return axios
+//         .get("https://randomuser.me/api")
+//         .then((result) => {
+//             data = result;
+//         })
+//         .catch(err => console.log(err));
+//     return data
+// }
+
+export const fetchUser = () => {
+    return axios.get("https://randomuser.me/api");
 }
 
 // export const fetchQuestion = () => async dispatch => {
