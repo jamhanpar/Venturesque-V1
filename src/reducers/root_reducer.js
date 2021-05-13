314be90a96a66e7caab02db758b640934c8bf5af
James <james.hansung.park@gmail.com>
2021-05-13 15:20:23 -0400

    ➕: added restaurants reducer

--- src/reducers/root_reducer.js
+++ src/reducers/root_reducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
 import { questionReducer } from "./question_reducer";
 import { userReducer } from "./user_reducer";
+import { restaurantReducer } from "./restaurant_reducer";
 
 export const rootReducer = combineReducers({
-    // restaurants
     // activities
     user: userReducer,
+    restaurants: restaurantReducer,
     questions: questionReducer
 })
