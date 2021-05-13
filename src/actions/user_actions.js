4add87eb86451cd175a191dee934b07428cc0050
James <james.hansung.park@gmail.com>
2021-05-13 15:15:16 -0400

    🔧: modified code format to utilize user util function

--- src/actions/user_actions.js
+++ src/actions/user_actions.js
@@ -1,15 +1,21 @@
 import { createAction } from '@reduxjs/toolkit';
 import axios from 'axios';
+import * as userUtils from '../util/users';
 
 export const receiveUser = createAction('RECEIVE_USER');
 
-export const fetchUser = () => async dispatch => {
-    try {
-        const res = await axios.get("https://randomuser.me/api");
-        console.log(res)
-        dispatch(receiveUser(res.data))
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+// export const fetchUser = () => async dispatch => {
+//     try {
+//         const res = await axios.get("https://randomuser.me/api");
+//         dispatch(receiveUser(res.data))
+//     }
+//     catch (err) {
+//         console.log(err)
+//     }
+// }
+
+export const fetchUser = () => dispatch => {
+    return userUtils.fetchUser()
+        .then(res => dispatch(receiveUser(res.data.results)))
+        .catch(err => console.log(err))
+};
