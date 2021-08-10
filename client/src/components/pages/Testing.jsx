13a03ef81432f5e1e924b7eb0350daa08f1d6841
James Park <james.hansung.park@gmail.com>
2021-08-10 14:18:00 -0400

    created testing page

---
+++ client/src/components/pages/Testing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Testing = (props) => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+// const msp = () => ({});
+
+// const mdp = dispatch => {};
+
+export default connect(null, null)(Testing);
