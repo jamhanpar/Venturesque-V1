e3cf433718414df12e6505a5dd1846cd20cd2cdf
James Park <james.hansung.park@gmail.com>
2021-08-10 13:38:49 -0400

    removed date input

--- client/src/components/Splash.jsx
+++ client/src/components/Splash.jsx
@@ -39,17 +39,17 @@ const Splash = (props) => {
         <section className='search-input'>
             <form onSubmit={handleSubmit}>
                 <div className='search-input-item'>
-                    <label className='landing-page-text' htmlFor='location'>the best <div className='typing-text'>first</div> date near </label>    
+                    <label className='landing-page-text' htmlFor='location'>the best first date near </label>    
                     <input className='landing-page-input' type='text' id='location' placeholder='address, city, zipcode' onChange={(e) => setLocation(e.target.value)} value={location}/>
                 </div>
                 {/* <div className='search-input-item'>
                     <label className='landing-page-text' htmlFor='friend'>with </label>
                     <input className='landing-page-input' type='text' id='friend' placeholder='friend' onChange={() => setFriend()} value={friend}/>
                 </div> */}
-                <div className='search-input-item'>
+                {/* <div className='search-input-item'>
                     <label className='landing-page-text' htmlFor='date'>on </label>
                     <input className='landing-page-date-input' type='date' id='date' placeholder='date' onChange={() => setDate()} value={date}/>
-                </div>
+                </div> */}
                 <div className='submit-button-container'>
                     <button className='generate-button' type='submit' onClick={handleSubmit}>Generate</button>
                 </div>
