865655e15734f9289a2a7e72723c2bf6c2637801
Jung Park <72415771+Junghyun2006@users.noreply.github.com>
2021-08-09 20:25:23 -0400

    test 3

---
+++ src/hooks/config.js
@@ -0,0 +1,3 @@
+let YELP_API_BASE_URL = '';
+let BEARER_TOKEN = '';
+let ACCU_API_KEY = 'BiWL0Qcw0XWr0YzB79UTc37dDx9c3UKW';
--- src/hooks/yelp-api/api.js
+++ src/hooks/yelp-api/api.js
@@ -1,9 +1,13 @@
+<<<<<<< HEAD
 import { API_BASE_URL, BEARER_TOKEN } from '../config';
+=======
+import { YELP_API_BASE_URL, BEARER_TOKEN } from '../../hooks/config';
+>>>>>>> 2b57ce7 (test)
 import queryString from 'query-string';
 
 export function get(path, queryParams) {
     const query = queryString.stringify(queryParams);
-    return fetch(`${API_BASE_URL}${path}?${query}`, {
+    return fetch(`${YELP_API_BASE_URL}${path}?${query}`, {
         mode: 'no-cors',
         headers: {
             Authorization: `Bearer ${BEARER_TOKEN}` ,
