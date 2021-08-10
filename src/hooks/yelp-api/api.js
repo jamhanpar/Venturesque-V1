72a9f23063d6a2f62f771df3af5731457cc251eb
Jung Park <72415771+Junghyun2006@users.noreply.github.com>
2021-08-09 20:30:46 -0400

    updated import yelp base url

--- src/hooks/yelp-api/api.js
+++ src/hooks/yelp-api/api.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { API_BASE_URL, BEARER_TOKEN } from '../config';
-=======
 import { YELP_API_BASE_URL, BEARER_TOKEN } from '../../hooks/config';
->>>>>>> 2b57ce7 (test)
 import queryString from 'query-string';
 
 export function get(path, queryParams) {
