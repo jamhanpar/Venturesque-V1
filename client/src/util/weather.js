cd1497ad336bda372d6897bf38e2e9011c162b58
James Park <james.hansung.park@gmail.com>
2021-08-12 13:39:39 -0400

    fixed weather api

--- client/src/util/weather.js
+++ client/src/util/weather.js
@@ -2,10 +2,9 @@ import axios from 'axios';
 
 export const fetchWeather = async (location) => {
     try {
-        const res = await axios.get(`/api/weather/location=${location}`);
-        debugger
-        console.log(res);
-        return res;
+        const res = await axios.get(`/api/accuweather/location=${location}`);
+        console.log(res.data);
+        return res.data;
     }
     catch (err) {
         console.log(err);
