f468319449ff08f7cd914f8f90f300c21009b6fa
James Park <james.hansung.park@gmail.com>
2021-08-25 13:19:02 -0400

    updating app format

--- client/src/components/Activities/Activity.jsx
+++ client/src/components/Activities/Activity.jsx
@@ -14,13 +14,13 @@ const Activity = ({ activity }) => {
   }, [activity]);
 
   return (
-    <div>
-      <div className="search-result-item-card">
+    <div className="search-result-item-card">
+      <div className="img-container">
         <img className="activity-img" src={photoURL} alt="business" />
-        <div className="item-info-container">
-          <h2 className="business-info">{activity.name}</h2>
-          <p className="business-info">{activity.rating} / {activity.user_ratings_total}</p>
-        </div>
+      </div>
+      <div className="item-info-container">
+        <h2 className="business-info">{activity.name}</h2>
+        <p className="business-info">{activity.rating} / {activity.user_ratings_total}</p>
       </div>
     </div>
   );
--- client/src/components/Restaurants/Restaurant.jsx
+++ client/src/components/Restaurants/Restaurant.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Restaurant = ({ restaurant }) => {
   return (
     <div className="search-result-item-card">
-      <img
-        className="restaurant-img"
-        src={restaurant.image_url}
-        alt="business"
-      />
+      <div className="img-container">
+        <img
+          className="restaurant-img"
+          src={restaurant.image_url}
+          alt="business"
+        />
+      </div>
       <div className="item-info-container">
         <h2 className="business-info">{restaurant.name}</h2>
         <p className="business-info">{restaurant.rating} / {restaurant.review_count} / {restaurant.price}</p>
