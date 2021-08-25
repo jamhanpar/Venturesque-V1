069c1df5ae04542b7735021ff3c99a65e62d6ac8
James Park <james.hansung.park@gmail.com>
2021-08-25 13:39:01 -0400

    created google maps component

---
+++ client/src/components/Map.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Map = () => {
+    let map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 40.7440, lng: -74.0324 },
+            zoom: 14,
+        });
+    }
+
+    return (
+        <div id="map"></div>
+    )
+}
+
+export default Map
