cd9d2cba1ccbd4d76a1c34cf3df7075b5147cc70
James <james.hansung.park@gmail.com>
2021-09-04 17:46:32 -0400

    user auth: signup works

--- server/app.js
+++ server/app.js
@@ -2,16 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 // user auth
+const cors = require('cors');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 // app
 const app = express();
 
+// user database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("UserDB is connected"))
+  .catch((err) => console.log(err));
+
 // database
 const db = require('./config/keys').mongoURI;
 
-// importing questions routes
+// import routes
 const questions = require('./routes/api/questions');
+const authRoutes = require('./routes/auth');
 
 // any additional routes import here
 const users = require('./routes/api/users');
@@ -21,20 +34,27 @@ const googlePhoto = require('./routes/api/googlePhoto');
 const accuweather = require('./routes/api/weather');
 const city = require('./routes/api/city');
 
-// parsing all incoming req and outgoing res to json
-app.use(express.json())
-
 // connecting to mongoDB using mongoose
-mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Mongo is connected'))
-    .catch(err => console.log(err))
+// mongoose
+//     .connect(db, { 
+//         useNewUrlParser: true,
+//         useCreateIndex: true, 
+//         useUnifiedTopology: true })
+//     .then(() => console.log('MongoDB is connected'))
+//     .catch(err => console.log(err))
+
+// middlewares
+// parsing all incoming req and outgoing res to json
+app.use(express.json());
+// app.use(bodyParser.json());
+app.use(cors());
 
 // create a route
 app.get('/', (req, res) => res.send("App is working!"))
 
-// using this router
+// routes middleware
 app.use('/api/questions', questions);
+app.use('/api', authRoutes);
 // any additional routes add here
 app.use('/api/users', users);
 app.use('/api/yelp', yelp);
