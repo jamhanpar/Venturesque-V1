b1ad1f10a816bf8c59fc3f851096338d1ce438bc
James <james.hansung.park@gmail.com>
2021-09-04 19:54:03 -0400

    updated css formatting

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -3,8 +3,8 @@ import Splash from '../Splash';
 
 const Home = () => {
     return (
-        <div className='Home'>
-            <Splash title="Home" />
+        <div className='home'>
+            <Splash />
         </div>
     )
 }
