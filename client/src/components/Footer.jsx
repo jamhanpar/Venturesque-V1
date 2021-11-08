33abe85762ab43b306259eecb4854680702a6bb5
James Park <james.hansung.park@gmail.com>
2021-11-07 21:59:27 -0500

    updated css

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            <div className='legal-copyright'>Copyright © 2021 Venturesque Inc. All rights reserved.</div>
-        </footer>
-    );
-}
+  return (
+    <footer className="footer">
+      <div className="legal-copyright">
+        Copyright © 2021 Venturesque Inc. All rights reserved.
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
