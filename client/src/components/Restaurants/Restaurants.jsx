e59608ddc969b7e51ae46a835df54caeb388f0f7
James Park <james.hansung.park@gmail.com>
2021-11-24 02:21:39 -0500

    added slider but ul html tag gets padding-left from _reboot.scss file and need to fix this issue

--- client/src/components/Restaurants/Restaurants.jsx
+++ client/src/components/Restaurants/Restaurants.jsx
@@ -1,40 +1,81 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Restaurant from './Restaurant';
-import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+import React from "react";
+import PropTypes from "prop-types";
+import Restaurant from "./Restaurant";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
-const Restaurants = ({restaurants, getBestRestaurant, setCurrentIdx, currentIdx}) => {
-    const filteredReviewCount = restaurants.filter(restaurant => restaurant.review_count >= 20);
-    const filteredRatings = filteredReviewCount.filter(restaurant => restaurant.rating >= 4);
-    const sortedByReviewCount = filteredRatings.sort((a, b) => b.review_count - a.review_count);
-    const sortedByRating = sortedByReviewCount.sort((a, b) => b.rating - a.rating);
+const Restaurants = ({
+  restaurants,
+  getBestRestaurant,
+  setCurrentIdx,
+  currentIdx,
+}) => {
+  const filteredReviewCount = restaurants.filter(
+    (restaurant) => restaurant.review_count >= 20
+  );
+  const filteredRatings = filteredReviewCount.filter(
+    (restaurant) => restaurant.rating >= 4
+  );
+  const sortedByReviewCount = filteredRatings.sort(
+    (a, b) => b.review_count - a.review_count
+  );
+  const sortedByRating = sortedByReviewCount.sort(
+    (a, b) => b.rating - a.rating
+  );
 
-    const restaurantIndex = sortedByRating.map((restaurant, idx) => (
-        <Restaurant key={ restaurant.id } restaurant={ restaurant } idx={ idx } setCurrentIdx={ setCurrentIdx }/>
-    ))
+  const restaurantIndex = sortedByRating.map((restaurant, idx) => (
+    <li class="item">
+      <Restaurant
+        key={restaurant.id}
+        restaurant={restaurant}
+        idx={idx}
+        setCurrentIdx={setCurrentIdx}
+      />
+    </li>
+  ));
 
-    //   const sortedRestaurants = restaurants.sort((a,b) => {
-    //       let aValue = (a.user_ratings_total < 15 || a.rating === 5) ? 0 : a.rating + a.user_ratings_total/100000
-    //       let bValue = (b.user_ratings_total < 15 || a.rating === 5) ? 0 : b.rating + b.user_ratings_total/100000
-    //       return bValue - aValue
-    //   })
+  //   const sortedRestaurants = restaurants.sort((a,b) => {
+  //       let aValue = (a.user_ratings_total < 15 || a.rating === 5) ? 0 : a.rating + a.user_ratings_total/100000
+  //       let bValue = (b.user_ratings_total < 15 || a.rating === 5) ? 0 : b.rating + b.user_ratings_total/100000
+  //       return bValue - aValue
+  //   })
 
-    if (getBestRestaurant) {
-        return (
-            <div className="search-results">
-                {restaurantIndex[currentIdx]}
-            </div>
-        )
-    } else {
-        return (
-            <div className="restaurant-list">
-                <FaAngleLeft onClick={() => setCurrentIdx(currentIdx > 0 ? currentIdx - 1 : 0)} />
-                {restaurantIndex.length > 10 ? restaurantIndex.slice(0, 6) : restaurantIndex}
-                <FaAngleRight onClick={() => setCurrentIdx(currentIdx < sortedByRating.length - 1 ? currentIdx + 1 : sortedByRating.length - 1)} />
-            </div>
-        )
-    }
-}
+  if (getBestRestaurant) {
+    return (
+      <div className="slider">
+        <ul id="autoWidth" class="cs-hidden">
+          {restaurantIndex[currentIdx]}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <div className="slider">
+        <ul id="autoWidth" class="cs-hidden">
+          {restaurantIndex.length > 10
+            ? restaurantIndex.slice(0, 6)
+            : restaurantIndex}
+        </ul>
+      </div>
+      //   <div className="restaurant-list">
+      //     <FaAngleLeft
+      //       onClick={() => setCurrentIdx(currentIdx > 0 ? currentIdx - 1 : 0)}
+      //     />
+      //     {restaurantIndex.length > 10
+      //       ? restaurantIndex.slice(0, 6)
+      //       : restaurantIndex}
+      //     <FaAngleRight
+      //       onClick={() =>
+      //         setCurrentIdx(
+      //           currentIdx < sortedByRating.length - 1
+      //             ? currentIdx + 1
+      //             : sortedByRating.length - 1
+      //         )
+      //       }
+      //     />
+      //   </div>
+    );
+  }
+};
 
 Restaurants.propTypes = {
   restaurants: PropTypes.array,
@@ -42,7 +83,6 @@ Restaurants.propTypes = {
   currentIdx: PropTypes.number,
   setCurrentIdx: PropTypes.func,
 };
-
 
 Restaurants.defaultProps = {
   getBestRestaurant: false,
