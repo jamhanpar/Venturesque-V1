59c4db71a0fe4707a2043f9be20abd11eff448e9
James <james.hansung.park@gmail.com>
2022-01-11 23:44:44 -0500

    🔨: got the collapsible menu to work, but running into display bugs when the menu opens

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/img/temp-logo.png";
 import { FaBars } from "react-icons/fa";
 
 export const Nav = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  console.log(toggleMenu);
+
   return (
     <div className="nav-container">
-      <Link to="/">
-        <img className="logo" src={logo} alt="App Logo" />
-      </Link>
-      <ul className="auth-nav-container">
-        <li className="logged-in">
+      <div className="home-link">
+        <Link to="/">
+          <img className="logo" src={logo} alt="App Logo" />
+        </Link>
+      </div>
+      <FaBars
+        className="toggle-button"
+        size={32}
+        onClick={() => setToggleMenu(!toggleMenu)}
+      />
+      <ul className={`auth-nav-container ${toggleMenu ? "active" : ""}`}>
+        <li>
           <Link className="auth-nav-link login-button" to="/about">
             About Us
           </Link>
         </li>
-        {/* <li className="logged-in">
+        {/* <li>
           <Link className="auth-nav-link login-button" to="/testing">
             Testing
           </Link>
         </li>
-        <li className="logged-in">
+        <li>
           <Link className="auth-nav-link login-button" to="/results-layout">
             Layout
           </Link>
         </li> */}
-        <li className="logged-in">
+        <li>
           <Link
             className="auth-nav-link login-button modal-trigger"
             data-target="modal-login"
@@ -34,13 +45,10 @@ export const Nav = () => {
             Login
           </Link>
         </li>
-        <li className="logged-in nav-button">
+        <li className="nav-button">
           <Link className="link-button" to="/auth/signup">
             Sign Up
           </Link>
-        </li>
-        <li>
-          <FaBars />
         </li>
       </ul>
     </div>
